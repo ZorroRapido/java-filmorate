bd40a8a087e0eac82ce0d312900ede11487c83d8
i.s.melnikov <i.s.melnikov@tinkoff.ru>
2022-12-06 17:16:30 +0300

    feat: реализовать контроллеры для пользователей и фильмов; написать тесты на данные контроллеры

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequestMapping("/films")
+public class FilmController {
+    private Map<Integer, Film> films = new HashMap<>();
+    private int counter = 1;
+
+    @PostMapping
+    private Film create(@Valid @RequestBody Film film) throws ValidationException {
+        validate(film);
+        film.setId(counter);
+        films.put(film.getId(), film);
+        counter++;
+        return film;
+    }
+
+    @PutMapping
+    private Film update(@Valid @RequestBody Film film) throws ValidationException, FilmNotFoundException {
+        if (!films.containsKey(film.getId())) {
+            log.warn("Фильм с id = " + film.getId() + " не найден!");
+            throw new FilmNotFoundException("Фильм с id = " + film.getId() + " не найден!");
+        }
+        validate(film);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @GetMapping
+    private Collection<Film> findAll() {
+        return films.values();
+    }
+
+    private void validate(Film film) throws ValidationException {
+        if (film.getDescription().length() > 200) {
+            log.warn("Превышено максимальное кол-во символов в описании!");
+            throw new ValidationException("Превышено максимальное кол-во символов в описании!");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, Month.DECEMBER, 28))) {
+            log.warn("Указанная дата релиза раньше дня рождения кино!");
+            throw new ValidationException("Указанная дата релиза раньше дня рождения кино!");
+        }
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private int id;
+    @NotNull @NotBlank private String name;
+    private String description;
+    private LocalDate releaseDate;
+    @Positive private int duration;
+
+    public Film(String name, String description, LocalDate releaseDate, int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
+}
